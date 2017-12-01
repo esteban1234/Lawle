1a5edcecea1c08b712e24b5c001b7ef397588e36
esteban1234 <tva_libra@hotmail.com>
2017-12-01 14:24:19 -0600

    pagina lista

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.jpg" />
+  <title>GRUPO LAWLE</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"> 
+    <META NAME="Title" content="GRUPO LAWLE">
+    <META NAME="Author" CONTENT="[http://grupolawle.com/]"/>
+    <META name="Description" content="Distribuimos camiones de basura, barredoras de playa y botes de basura para el cuidado del medio ambiente con fines de mantener un entorno sustentable. Ademas contamos con productos industriales y de limpieza municipal, productos de señalizacion vial, coladeras y articulos policiacos."/>
+    <META NAME="Keywords" CONTENT="camiones de basura, barredoras de playas, Equipo de Bacheo, contenedores basura, recolectores de basura, recoleccion de basura, camiones recolectores basura, botes de basura, recoleccion de desechos solidos, contenedores para basura, contenedores de basura, botes para basura, limpieza de playas, limpieza de arena, barredoras para arena, recolectores de desechos solidos, vehiculos recolectores de basura, venta de camiones recolectores de basura, recoleccion de basura, recoleccion basura, contenedores de basura industriales."/>
+
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Roboto|Timmana|Source+Sans+Pro|Contrail+One|Spectral+SC|Lora" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+    
+
+
+<div class="bannerh" id="INICIO">
+  <div class="header_top"></div>
+
+  <div class="menu">
+    <figure class="logo pull-right">
+      <img src="img/lawle.png" alt="LAWLE" class="img-responsive">
+    </figure>
+
+    <nav class="pull-left">
+       <ul class="">
+          <li class=""><a class="selector" data-scroll href="#INICIO" aria-pressed="true" id="active">INICIO</a></li>
+          <li class=""><a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a></li>
+          <li class=""><a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true" >SERVICIOS</a></li>
+          <li class=""><a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a></li>
+
+        </ul>
+    </nav>
+  </div>
+
+
+<div class="banner">
+  <div class="fondo">
+    <h1>MANEJO DE RESIDUOS</h1>
+    <p>EL ORDEN QUE SE DEBE DE MANEJAR LOS RESIDUOS</p>
+     <div style="text-align: center; margin-top: 5%;">
+      <a class="btn btn-danger btn-outline btn-lg"  href="#CONTACTO" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus" aria-hidden="true"></i> Información</a>
+  </div>
+  </div>
+</div>
+
+</div>
+
+
+<section class="seccion1" id="NOSOTROS">
+  <h3>APORTAMOS ALTERNATIVAS PARA EL MEJORAMIENTO CON SISTEMAS DE TRANSPORTE, TRATAMIENTO Y RECICLAJE Y DISPOSICIÓN FINAL DE RESIDUOS.</h3> <br><br>
+
+  <div class="section">
+      <div class="container">
+        <div class="row">
+            <div class="imge">
+              
+            </div>
+            <div class="imge2">
+              
+            </div>
+            <div class="imge3">
+              
+            </div>
+          
+        </div>
+      </div>
+    </div>
+      <br><br>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h1>GRUPO LAWLE</h1>
+            <h3 style="text-align: left;">Manejo integral de residuos</h3>
+            <p>
+            LAWLE es una organización dedicada a la gestión integral de excedentes industriales y residuos peligrosos, coherente con su misión, visión, principios y valores, considera como factores de gran importancia la satisfacción de nuestros clientes, la seguridad de nuestros procesos, la salud de nuestros trabajadores, el cuidado de nuestro medio ambiente y la responsabilidad social. <br><br>
+
+            Tenemos un compromiso inquebrantable a convertirnos en una empresa líder en la prestación de servicios integrales en el manejo de los residuos industriales peligrosos y no peligrosos. Contamos con una infraestructura de primer nivel y con personal altamente capacitado y comprometido con la protección del medio ambiente.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="img/qumicos.jpg"
+            class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+
+<section class="seccion3" id="SERVICIOS">
+  <h1 class="text-center" style="color: #fff;">SERVICIOS</h1>
+    <p class="text-center" style="color: #fff;">Liderazgo en recolección y disposición de residuos.</p> <br><br>
+  <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <div class="image"></div>
+            <h2 class="text-center" style="color: #fff;">Recolección de Residuos No Peligrosos</h2>
+            <p class="text-center" style="color: #fff;">En LAWLE proveemos soluciones para los problemas de manejo  de residuos sólidos urbanos a través de nuestros servicios de recolección, transporte y disposición final. </p>
+          </div>
+          <div class="col-md-4">
+            <div class="image2"></div>
+            <h2 class="text-center" style="color: #fff;">Recolección de Residuos Peligrosos Biológico Infecciosos</h2>
+            <p class="text-center" style="color: #fff;">Contamos con experiencia en el manejo integral de Residuos Peligrosos Biológico Infecciosos cumpliendo estrictamente con la normatividad aplicable a la materia.</p>
+          </div>
+          <div class="col-md-4">
+            <div class="image3"></div>
+            <h2 class="text-center" style="color: #fff;">Recolección de Residuos Industriales con clave CRETI</h2>
+            <p class="text-center" style="color: #fff;">Contamos con programas para el manejo y control de residuos industriales con clave CRETI generados por las actividades de diversas empresas, industrias e instituciones.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class="seccion4" id="CONTACTO">
+  <h1 class="text-center" style="color: #000;">CONTACTO</h1>
+    <p class="text-center" style="color: #000;">Contáctenos, estaremos siempre a su mejor disposición.</p> <br><br>
+
+    <section id="contact">
+  <div class="container">
+  
+  <div class="row">
+    <div class="col-md-7">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3821.4218147902316!2d-93.17891988575732!3d16.705792126502338!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ecd9f928130d57%3A0xab38da7211a4919a!2sBlvd.+Bonanza%2C+Residencial+Bonanza%2C+29050+Tuxtla+Guti%C3%A9rrez%2C+Chis.!5e0!3m2!1ses-419!2smx!4v1512159583850" width="100%" height="315" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <div class="col-md-5">
+          <h4><strong>Por favor llene el siguiente formulario</strong></h4>
+        <form>
+          <div class="form-group">
+            <input type="text" class="form-control" name="" value="" placeholder="Escribe tu nombre completo">
+          </div>
+          <div class="form-group">
+            <input type="email" class="form-control" name="" value="" placeholder="Escribe tu correo completo">
+          </div>
+          <div class="form-group">
+            <input type="tel" class="form-control" name="" value="" placeholder="Escribe tu teléfono completo">
+          </div>
+          <div class="form-group">
+            <textarea class="form-control" name="" rows="3" placeholder="Escribe tu comentario"></textarea>
+          </div>
+          <button class="btn btn-default" type="submit" name="button">
+              <i class="fa fa-paper-plane-o" aria-hidden="true"></i> ENVIAR
+          </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+</section>
+
+<!-- <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+        </div>
+        <div class="modal-body">
+          <h3 style="margin-top: 1%;margin-bottom: 3%; text-align: center;">Nuestros asesores le darán toda la información que necesita ¡CONTACTANOS AHORA!</h3>
+          <form id="contact" method="post" class="form" role="form">
+<div class="row">
+<div class=" col-md-12 form-group">
+<input class="form-control" id="name" name="name" placeholder="Escribe tu nombre completo" type="text" required  />
+</div>
+<div class=" col-md-12 form-group">
+<input class="form-control" id="name" name="name" placeholder="Escribe tu número teléfonico" type="text" required  />
+</div>
+<div class=" col-md-12 form-group">
+<input class="form-control" id="email" name="email" placeholder="Escribe tu correo" type="email" required />
+</div>
+</div>
+<textarea class="form-control" id="message" name="message" placeholder="Escribe tu comentario" rows="5"></textarea>
+<br />
+<div class="row">
+<div class="col-xs-12 col-md-12 form-group">
+<button class="btn btn-success pull-right" type="submit">ENVIAR</button>
+</div>
+</div>
+</form>
+        </div>
+      </div>
+      
+    </div>
+  </div> -->
+
+<!--   <div class="modal fade" id="img-content" role="dialog">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-body modal-imgb">
+          <img src="img/terrenos.jpg" class="img-responsive img-modal" alt="">
+        </div>
+      </div>
+      
+    </div>
+  </div> -->
+
+
+ 
+
+<footer>
+<p>Derechos de autor 2017 © | GRUPO LAWLE SA DE CV </p>
+</footer>
+
+<span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- <script src="js/jquery.js"></script> -->
+    <script src="js/arriba.js"></script>
+    <!-- <script src="js/bootstrap.js"></script> -->
+    <script src="js/menu.js"></script>
+  </body>
+</html>
