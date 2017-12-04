df797e3b50e9c4b3390f6e62d6d190bee19b0342
esteban1234 <tva_libra@hotmail.com>
2017-12-04 16:53:16 -0600

    metas

--- envia.php
+++ envia.php
@@ -22,8 +22,7 @@ $mail->From = $correo;
 $mail->FromName = $nombre;
 
 //To address and name
-// $mail->addAddress("info@grupolawle.com");
-$mail->addAddress("ballina.santiago@gmail.com");
+$mail->addAddress("info@grupolawle.com");
 
 
 
